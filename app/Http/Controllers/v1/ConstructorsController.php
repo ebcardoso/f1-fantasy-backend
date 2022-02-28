c96b03e7317d02866a6eed01efb976c2199fcbff
Breno Cardoso <elisiobreno@gmail.com>
2022-02-28 12:32:15 -0300

    refactor: Constructors Form Request and QueryException

--- app/Http/Controllers/v1/ConstructorsController.php
+++ app/Http/Controllers/v1/ConstructorsController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\QueryException;
 use App\Models\v1\ConstructorsModel as ConstructorsModel;
+use App\Http\Requests\v1\ConstructorsFormRequest as ConstructorsFormRequest;
 
 class ConstructorsController extends Controller
 {
@@ -14,18 +15,13 @@ class ConstructorsController extends Controller
         try {
             $constructors = ConstructorsModel::All();
             return response()->json($constructors, 200);
-        } catch (Exception $e) {
-            return response()->json([], 500);
+        } catch (QueryException $ex) {
+            return response()->json($ex, 500);
         }
     }
 
-    public function store(Request $request)
+    public function store(ConstructorsFormRequest $request)
     {
-        $validator = Validator::make($request->all(), $this->get_rules(), $this->get_messages());
-        if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors()], 400);
-        }
-
         $constructor = new ConstructorsModel;
         $constructor->name = $request->name;
         $constructor->country = $request->country;
@@ -39,8 +35,8 @@ class ConstructorsController extends Controller
         try {
             $constructor->save();
             return response()->json($constructor, 201);
-        } catch (Exception $e) {
-            return response()->json(['errors' => $e->getMessage()], 500);
+        } catch (QueryException $ex) {
+            return response()->json($ex, 500);
         }
     }
 
@@ -53,23 +49,18 @@ class ConstructorsController extends Controller
             } else {
                 return response()->json($constructor, 200);
             }
-        } catch (Exception $e) {
-            return response()->json([], 500);
+        } catch (QueryException $ex) {
+            return response()->json($ex, 500);
         }
     }
 
-    public function update(Request $request, $id)
+    public function update(ConstructorsFormRequest $request, $id)
     {
         $constructor = ConstructorsModel::find($id);
         if (is_null($constructor)) {
             return response()->json(['errors' => 'Constructor Not Found!'], 404);
         }
         
-        $validator = Validator::make($request->all(), $this->get_rules(), $this->get_messages());
-        if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors()], 400);
-        }
-
         $constructor->name = $request->name;
         $constructor->country = $request->country;
         $constructor->founded_in = $request->founded_in;
@@ -82,8 +73,8 @@ class ConstructorsController extends Controller
         try {
             $constructor->save();
             return response()->json($constructor, 200);
-        } catch (Exception $e) {
-            return response()->json(['errors' => "Erro ao Editar",], 500);
+        } catch (QueryException $ex) {
+            return response()->json($ex, 500);
         }
     }
 
@@ -97,38 +88,12 @@ class ConstructorsController extends Controller
                 try {
                     $delete = $constructor->delete();
                     return response()->json($constructor, 200);
-                } catch (Exception $e) {
-                    return response()->json(['errors' => "Erro ao Deletar",], 500);
+                } catch (QueryException $ex) {
+                    return response()->json($ex, 500);
                 }
             }
-        } catch (Exception $e) {
-            return response()->json(['error' => "Erro ao Deletar",], 500);
+        } catch (QueryException $ex) {
+            return response()->json($ex, 500);
         }
-    }
-
-    private function get_rules() {
-        return [
-            'name' => 'required',
-            'country' => 'required',
-            'founded_in' => 'required',
-            'engine' => 'required',
-            'color_primary' => 'required',
-            'color_secondary' => 'required',
-            'url_logo' => 'required',
-            'url_icon' => 'required'
-        ];
-    }
-
-    private function get_messages() {
-        return [
-            'name.required' => 'Param <name> is required',
-            'country.required' => 'Param <country> is required',
-            'founded_in.required' => 'Param <founded_in> is required',
-            'engine.required' => 'Param <engine> is required',
-            'color_primary.required' => 'Param <color_primary> is required',
-            'color_secondary.required' => 'Param <color_secondary> is required',
-            'url_logo.required' => 'Param <url_logo> is required',
-            'url_icon.required' => 'Param <url_icon> is required'
-        ];
     }
 }
--- app/Http/Requests/v1/ConstructorsFormRequest.php
+++ app/Http/Requests/v1/ConstructorsFormRequest.php
@@ -1,16 +1,11 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\v1;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class ConstructorsRequest extends FormRequest
+class ConstructorsFormRequest extends FormRequest
 {
-    public function authorize()
-    {
-        return true;
-    }
-
     public function rules()
     {
         return [
